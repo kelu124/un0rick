3c23ad7427569d25e06861d360ce2ba78d658636
kelu124 <kelu124@gmail.com>
2020-07-26 15:01:45 +0200

    Update hardware, need to push VHDL + bin

--- software/pll248M2_inst.vhd
+++
@@ -1,9 +0,0 @@
-pll248M2_inst: pll248M2
-port map(
-          REFERENCECLK => ,
-          PLLOUTCOREA => ,
-          PLLOUTCOREB => ,
-          PLLOUTGLOBALA => ,
-          PLLOUTGLOBALB => ,
-          RESET => 
-        );
--- vhdl/MATTY serie 20181125/MATTY serie/pll256M2_inst.vhd
+++
@@ -1,9 +0,0 @@
-pll256M2_inst: pll256M2
-port map(
-          REFERENCECLK => ,
-          PLLOUTCOREA => ,
-          PLLOUTCOREB => ,
-          PLLOUTGLOBALA => ,
-          PLLOUTGLOBALB => ,
-          RESET => 
-        );
